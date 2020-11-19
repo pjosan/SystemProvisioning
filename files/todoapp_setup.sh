ea794ac9089a7d82958f77a62092489d33530b71
pjosan <71610609+pjosan@users.noreply.github.com>
2020-11-18 17:12:59 -0800

    Add files via upload

---
+++ files/todoapp_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+APP="/home/todoapp/app"
+REPO_URL="https://github.com/timoguic/ACIT4640-todo-app.git"
+
+git clone $REPO_URL $APP
+npm install --prefix $APP
+sed -i -e 's/CHANGEME/acit4640' "${APP}/config/database.js"
+chmod a+rx /home/todoapp
